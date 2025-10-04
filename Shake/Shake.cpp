f0e49804882eb3088ac3a4736bbcc67eafc954da
ITTM <asinmaxim14@gmail.com>
2025-10-04 18:36:45 +0700

    Update

--- Shake/Shake.cpp
+++ Shake/Shake.cpp
@@ -2,10 +2,8 @@
 #include <iostream>
 #include <vector>
 
-
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
     std::vector<HWND>* windows = reinterpret_cast<std::vector<HWND>*>(lParam);
-
 
     if (IsWindowVisible(hwnd) && GetParent(hwnd) == NULL) {
         windows->push_back(hwnd);
@@ -15,10 +13,8 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
 }
 
 int main() {
-
     std::vector<HWND> windows;
     EnumWindows(EnumWindowsProc, reinterpret_cast<LPARAM>(&windows));
-
 
     std::vector<POINT> originalPositions;
     std::vector<SIZE> originalSizes;
@@ -30,11 +26,7 @@ int main() {
         originalSizes.push_back({ rect.right - rect.left, rect.bottom - rect.top });
     }
 
-    int screenW = GetSystemMetrics(SM_CXSCREEN);
-    int screenH = GetSystemMetrics(SM_CYSCREEN);
-
     while (true) {
-
         ULONGLONG startTime = GetTickCount64();
         while (GetTickCount64() - startTime < 10000) {
             for (size_t i = 0; i < windows.size(); i++) {
@@ -49,42 +41,6 @@ int main() {
             }
             Sleep(30);
         }
-
-
-        for (size_t i = 0; i < windows.size(); i++) {
-            SetWindowPos(windows[i], 0,
-                originalPositions[i].x,
-                originalPositions[i].y,
-                originalSizes[i].cx,
-                originalSizes[i].cy,
-                SWP_NOZORDER);
-        }
-
-
-        startTime = GetTickCount64();
-        std::vector<POINT> currentPositions(windows.size(), POINT{ 0, 0 });
-        std::vector<POINT> directions(windows.size(), POINT{ 3, 2 });
-
-        while (GetTickCount64() - startTime < 10000) {
-            for (size_t i = 0; i < windows.size(); i++) {
-                currentPositions[i].x += directions[i].x;
-                currentPositions[i].y += directions[i].y;
-
-                if (currentPositions[i].x <= 0 || currentPositions[i].x + originalSizes[i].cx >= screenW)
-                    directions[i].x = -directions[i].x;
-                if (currentPositions[i].y <= 0 || currentPositions[i].y + originalSizes[i].cy >= screenH)
-                    directions[i].y = -directions[i].y;
-
-                SetWindowPos(windows[i], 0,
-                    currentPositions[i].x,
-                    currentPositions[i].y,
-                    originalSizes[i].cx,
-                    originalSizes[i].cy,
-                    SWP_NOZORDER);
-            }
-            Sleep(10);
-        }
-
 
         for (size_t i = 0; i < windows.size(); i++) {
             SetWindowPos(windows[i], 0,
